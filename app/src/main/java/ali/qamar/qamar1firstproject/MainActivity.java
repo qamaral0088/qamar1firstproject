98bae87e3e048a0df96870cb8b475052fa2ae7af
Admin <WIPES.6098@GMAIL.COM>
2023-11-19 10:41:53 +0200

    Initial commit

---
+++ app/src/main/java/ali/qamar/qamar1firstproject/MainActivity.java
@@ -0,0 +1,33 @@
+package ali.qamar.qamar1firstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+private TextView tv_outout;
+private Button btn_Go;
+private EditText et_input;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv_outout=findViewById(R.id.tvOutput);
+        et_input=findViewById(R.id.etlnput);
+        btn_Go =findViewById(R.id. btnGo);
+
+    }
+
+
+        public void onClickGo (View  v) {
+            String st = et_input.getText().toString();
+            tv_outout.setTex(st);
+        }
+    
+
+        }
---
+++ app/src/main/java/ali/qamar/qamar1firstproject/secondActivity.java
@@ -0,0 +1,19 @@
+package ali.qamar.qamar1firstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class secondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+
+}
