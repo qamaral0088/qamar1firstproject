3563b90a0e642e7f4deafef32ab24e6c89b160df
Admin <WIPES.6098@GMAIL.COM>
2023-12-03 11:36:39 +0200

    حطينا كود

---
+++ app/src/main/java/ali/qamar/qamar1firstproject/data/UsersTable/MyUser.java
@@ -0,0 +1,31 @@
+package ali.qamar.qamar1firstproject.data.UsersTable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+public class MyUser
+{
+    @PrimaryKey(autoGenerate = true)//تحديد الصفة كمفتاح رئيسي والذي يُنتجح بشكل تلقائي
+    public long keyid;
+    @ColumnInfo(name = "full_Name")//اعطاء اسم جديد للعامود-الصفة في الجدول
+    public String fullName;
+    public String email;//بحالة لم يتم اعطاء اسم للعامود يكون اسم الصفه هو اسم العامود
+    public String phone;
+    public String passw;
+
+
+    @Override
+    public String toString() {
+        return "MyUser{" +
+                "keyid=" + keyid +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", passw='" + passw + '\'' +
+                '}';
+
+
+        }
+
+    }
