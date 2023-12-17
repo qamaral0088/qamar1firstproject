2f0442bc40a93bfa9b8387ebf1878ae47690f1b4
Admin <WIPES.6098@GMAIL.COM>
2023-12-17 10:32:01 +0200

    حطينا كود

--- app/src/main/java/ali/qamar/qamar1firstproject/data/UsersTable/MyUsersQuery.java
+++ app/src/main/java/ali/qamar/qamar1firstproject/data/UsersTable/MyUsersQuery.java
@@ -1,17 +1,14 @@
 package ali.qamar.qamar1firstproject.data.UsersTable;
 
 import androidx.room.Dao;
+import androidx.room.Entity;
 import androidx.room.Query;
 
 import java.util.List;
 @Dao
+@Entity
+
 public interface MyUsersQuery
 {
-    @Query (" SElECT* FROM MyUser")
-    List<MyUser> getAll();
-    @Query ("SElECT*FROM  MyUser  WHERE keyid IN (:userIds)")
-    List<MyUser> loadALLByIds(int[]userIds);
-    @Query ("SElECT WHERE email*FROM MyUser =  :myEmail AND passw=:myPassw LIMIT 1")
-    MyUser checkEmailpassw(String myEmail,String myPassw);
-
+  @Query("SELECT * FROM MyUser")
 }
