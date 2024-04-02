7ad22e7fb07f80370a5b9a3aaf353e366b0c4e01
Admin <WIPES.6098@GMAIL.COM>
2024-04-02 12:01:37 +0300

    اكمال الصفحه   الثانيه   جعل مسافه بين كل خانه......

--- app/src/main/java/ali/qamar/qamar1firstproject/SignUpActivity.java
+++ app/src/main/java/ali/qamar/qamar1firstproject/SignUpActivity.java
@@ -3,12 +3,11 @@ package ali.qamar.qamar1firstproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class
-SignInActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
     }
 }
