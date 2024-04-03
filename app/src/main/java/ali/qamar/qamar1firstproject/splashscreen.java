f554986e16534d6c6e3c7a604f8a330b016852ae
Admin <WIPES.6098@GMAIL.COM>
2024-04-03 11:23:57 +0300

    اكمال الصفحه 3  عمل ال spener

---
+++ app/src/main/java/ali/qamar/qamar1firstproject/splashscreen.java
@@ -0,0 +1,21 @@
+package ali.qamar.qamar1firstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+    }
+
+
+
+    
+
+        }
